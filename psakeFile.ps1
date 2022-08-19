737e07131af2d9eac4e351b1904fe37c31c1521c
Dylan Prins <8777848+Dylan-Prins@users.noreply.github.com>
2022-08-19 16:03:00 +0200

    added release

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -8,3 +8,16 @@ properties {
 task Default -depends Test
 
 task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+
+task Publishing -depends Test -action {
+    Set-Location $env:BHBuildOutput
+    nuget spec $env:BHProjectName
+
+    [xml]$nuspec = Get-Content "$env:BHProjectName.nuspec"
+    $nuspec.package.metadata.version = $Version
+    $nuspec.Save("$env:BHBuildOutput/$env:BHProjectName.nuspec")
+
+    nuget pack $env:BHProjectName.nuspec
+    nuget sources Add -Name "AzureDevOps" -Source "https://pkgs.dev.azure.com/weareinspark/_packaging/$FeedName/nuget/v3/index.json" -username $Username -password $PAT
+    nuget push -Source "AzureDevOps" -ApiKey $PAT "$env:BHBuildOutput/InfraStructureAsCode.$Version.nupkg"
+}
