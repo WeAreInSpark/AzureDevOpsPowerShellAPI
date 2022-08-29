2c0c148fc7757763cd6cec496c545f8366f420a4
Christian Piet <christian.piet@inspark.nl>
2022-08-29 12:32:27 +0000

    Edit Get cmdlets

--- InSpark.InfrastructureAsCode/Public/Get-AzDoRepo.ps1
+++ InSpark.InfrastructureAsCode/Public/Get-AzDoRepo.ps1
@@ -24,6 +24,15 @@ function Get-AzDoRepo {
     Get-AzDoRepo -CollectionUri = "https://dev.azure.com/contoso" -PAT = "***" -ProjectName = "Project 1" -Name "Repo 1"
 
     This example will fetch information about the repo with the name 'Repo 1'.
+.EXAMPLE
+    $Params = @{
+        CollectionUri = "https://dev.azure.com/contoso"
+        PAT = "***"
+        Name "Repo 1"
+    }
+    get-AzDoProject -pat $pat -CollectionUri $collectionuri | Get-AzDoRepo -PAT $PAT
+
+    This example will fetch information about the repo with the name 'Repo 1'.
 .OUTPUTS
     PSObject with repo(s).
 .NOTES
@@ -31,7 +40,7 @@ function Get-AzDoRepo {
     [CmdletBinding()]
     param (
         # Collection Uri of the organization
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [string]
         $CollectionUri,
 
@@ -41,23 +50,22 @@ function Get-AzDoRepo {
         $PAT = $env:SYSTEM_ACCESSTOKEN,
 
         # Name of the Repo to get information about
-        [Parameter()]
+        [Parameter(ParameterSetName = 'Get')]
         [string]
         $Name,
 
         # Project where the Repos are contained
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [string]
         $ProjectName
     )
-    Begin {
+    Process {
         if ($Name) {
             $uri = "$CollectionUri/$ProjectName/_apis/git/repositories/$($Name)?api-version=7.1-preview.1"
         } else {
             $uri = "$CollectionUri/$ProjectName/_apis/git/repositories?api-version=7.1-preview.1"
         }
-    }
-    Process {
+
         $params = @{
             uri         = $uri
             Method      = 'GET'
@@ -65,6 +73,38 @@ function Get-AzDoRepo {
             ContentType = 'application/json'
         }
 
-        Invoke-RestMethod @params
+        if ($name) {
+        (Invoke-RestMethod @params) | ForEach-Object {
+                [PSCustomObject]@{
+                    RepoName      = $_.name
+                    RepoID        = $_.id
+                    RepoURL       = $_.url
+                    ProjectName   = $ProjectName
+                    DefaultBranch = $_.defaultBranch
+                    WebUrl        = $_.webUrl
+                    HttpsUrl      = $_.remoteUrl
+                    SshUrl        = $_.sshUrl
+                    CollectionURI = $CollectionUri
+                    IsDisabled    = $_.IsDisabled
+                }
+            }
+        } else {
+            (Invoke-RestMethod @params).value | ForEach-Object {
+                [PSCustomObject]@{
+                    RepoName      = $_.name
+                    RepoID        = $_.id
+                    RepoURL       = $_.url
+                    ProjectName   = $ProjectName
+                    DefaultBranch = $_.defaultBranch
+                    WebUrl        = $_.webUrl
+                    HttpsUrl      = $_.remoteUrl
+                    SshUrl        = $_.sshUrl
+                    CollectionURI = $CollectionUri
+                    IsDisabled    = $_.IsDisabled
+                }
+            }
+        }
     }
 }
+
+Get-AzDoProject -PAT $PAT -CollectionUri $collectionURI -ProjectName DitProject | Get-AzDoRepo -PAT $PAT
