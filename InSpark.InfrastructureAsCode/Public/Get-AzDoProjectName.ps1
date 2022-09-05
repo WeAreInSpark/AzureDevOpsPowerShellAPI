e9feb99c0946f89a5c620a15165d833a22476086
Lucas-c-B <lcbahnmuller@gmail.com>
2022-09-05 16:45:17 +0200

    Added initial version

---
+++ InSpark.InfrastructureAsCode/Public/Get-AzDoProjectName.ps1
@@ -0,0 +1,50 @@
+function Get-AzDoProjectName {
+    <#
+.SYNOPSIS
+    Gets the name of a project by using the project ID.
+.DESCRIPTION
+    Gets the name of a project by using the project ID.
+.EXAMPLE
+    $params = @{
+        CollectionUri       = "https://dev.azure.com/contoso"
+        PAT                 = "***"
+        ProjectId           = "6ed46fc1-9152-4a63-8817-530fd24b1662"
+    }
+    Get-AzDoProjectName @params
+
+    This example gets the name of the project with ID "6ed46fc1-9152-4a63-8817-530fd24b1662"
+.OUTPUTS
+    string
+    ProjectName
+.NOTES
+#>
+    [CmdletBinding()]
+    param (
+        # Collection Uri of the organization
+        [Parameter(Mandatory)]
+        [string]
+        $CollectionUri,
+
+        # PAT to authenticate with the organization
+        [Parameter(Mandatory)]
+        [string]
+        $PAT,
+
+        # ID of the project
+        [Parameter(Mandatory)]
+        [string]
+        $ProjectId
+    )
+
+    $params = @{
+        uri         = "$CollectionUri/_apis/projects/$($ProjectId)?api-version=7.1-preview.4"
+        Method      = 'GET'
+        Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+        ContentType = 'application/json'
+
+    }
+    $response = Invoke-RestMethod @params
+    $ProjectName = $response.name
+
+    $ProjectName
+}
