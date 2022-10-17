5ef90cad5c9b55284905fd1cbb2c5a3fd8457bbb
Lucas-c-B <lcbahnmuller@gmail.com>
2022-10-17 15:34:29 +0200

    Fix temp file error

--- InSpark.InfrastructureAsCode/Public/New-AzDoRepoClone.ps1
+++ InSpark.InfrastructureAsCode/Public/New-AzDoRepoClone.ps1
@@ -98,16 +98,13 @@ function New-AzDoRepoClone {
 
             git push --mirror "https://$DestinationPAT@dev.azure.com/$DestinationOrganizationName/$DestinationProjectName/_git/$DestinationRepoName"
         } else {
-            $Path = (Get-Location).Path
-            New-Item -Path (Split-Path -Parent -Path $Path) -Name 'TempScriptBaseline' -ItemType Directory
-            Set-Location (Join-Path -ChildPath 'TempScriptBaseline' -Path (Split-Path -Parent -Path $Path))
+            $TempFile = New-TemporaryFile
 
             git clone "https://$SourcePAT@dev.azure.com/$SourceOrganizationName/$SourceProjectName/_git/$SourceRepoName" --branch main
             Set-Location $SourceRepoName
             git push "https://$DestinationPAT@dev.azure.com/$DestinationOrganizationName/$DestinationProjectName/_git/$DestinationRepoName"
 
-            Set-Location $Path
-            Remove-Item -Recurse -Force (Join-Path -ChildPath 'TempScriptBaseline' -Path (Split-Path -Parent -Path $Path))
+            Remove-Item -Recurse -Force $TempFile
         }
 
         $getAzDoRepoSplat = @{
