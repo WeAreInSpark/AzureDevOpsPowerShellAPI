9fde5f5472795f7bb34126dc73d6f51562f2555d
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2023-10-11 14:24:21 +0200

    Fixed Repo

--- InSpark.InfrastructureAsCode/Public/New-AzDoRepo.ps1
+++ InSpark.InfrastructureAsCode/Public/New-AzDoRepo.ps1
@@ -19,7 +19,7 @@ function New-AzDoRepo {
     Containg the repo information
 .NOTES
 #>
-    [CmdletBinding(SupportsShouldProcess)]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
         # Collection Uri of the organization
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
@@ -36,13 +36,53 @@ function New-AzDoRepo {
         [string]
         $RepoName,
 
+        # Switch to use PAT instead of OAuth
+        [Parameter()]
+        [switch]
+        $UsePAT = $false,
+
         # Name of the project where the new repository has to be created
         [Parameter(Mandatory)]
         [string]
         $ProjectName
     )
+    Begin {
+        if ($UsePAT) {
+            Write-Verbose 'The [UsePAT]-parameter was set to true, so the PAT will be used to authenticate with the organization.'
+            if ($PAT -eq $env:SYSTEM_ACCESSTOKEN) {
+                Write-Verbose -Message "Using the PAT from the environment variable 'SYSTEM_ACCESSTOKEN'."
+            } elseif (-not [string]::IsNullOrWhitespace($PAT) -and $PSBoundParameters.ContainsKey('PAT')) {
+                Write-Verbose -Message "Using a custom PAT supplied in the parameters."
+            } else {
+                try {
+                    throw "Requested to use a PAT, but no custom PAT was supplied in the parameters or the environment variable 'SYSTEM_ACCESSTOKEN' was not set."
+                } catch {
+                    $PSCmdlet.ThrowTerminatingError($_)
+                }
+            }
+        } else {
+            Write-Verbose 'The [UsePAT]-parameter was set to false, so an OAuth will be used to authenticate with the organization.'
+            $PAT = ($UsePAT ? $PAT : $null)
+        }
+        try {
+            $Header = New-ADOAuthHeader -PAT $PAT -AccessToken:($UsePAT ? $false : $true) -ErrorAction Stop
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
     process {
-        $Projects = Get-AzDoProject -CollectionUri $CollectionUri -PAT $PAT
+        $getAzDoProjectSplat = @{
+            CollectionUri = $CollectionUri
+        }
+
+        if ($PAT) {
+            $getAzDoProjectSplat += @{
+                PAT    = $PAT
+                UsePAT = $true
+            }
+        }
+
+        $Projects = Get-AzDoProject @getAzDoProjectSplat
         $ProjectId = ($Projects | Where-Object ProjectName -EQ $ProjectName).Projectid
 
         $Body = @{
@@ -55,12 +95,12 @@ function New-AzDoRepo {
         $params = @{
             uri         = "$CollectionUri/$ProjectId/_apis/git/repositories?api-version=7.1-preview.1"
             Method      = 'POST'
-            Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
+            Headers     = $Header
             body        = $Body | ConvertTo-Json -Depth 99
             ContentType = 'application/json'
         }
 
-        if ($PSCmdlet.ShouldProcess($CollectionUri)) {
+        if ($PSCmdlet.ShouldProcess($ProjectName, "Create repo named: $($PSStyle.Bold)$Reponame$($PSStyle.Reset)")) {
             Invoke-RestMethod @params | ForEach-Object {
                 [PSCustomObject]@{
                     RepoName      = $_.name
