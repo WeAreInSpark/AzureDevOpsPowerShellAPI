81b320947cefa1afc2d3a466ed1eef0206029b61
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2023-10-25 14:00:16 +0200

    Add fork

--- InSpark.InfrastructureAsCode/Public/New-AzDoRepoFork.ps1
+++ InSpark.InfrastructureAsCode/Public/New-AzDoRepoFork.ps1
@@ -38,25 +38,94 @@ function New-AzDoRepoFork {
         [String]
         $PAT = $env:SYSTEM_ACCESSTOKEN,
 
-        # Project where the variable group has to be created
+        # Switch to use PAT instead of OAuth
+        [Parameter()]
+        [switch]
+        $UsePAT = $false,
+
+        # Project where the fork has to be created
         [Parameter(Mandatory)]
         [string]
-        $ProjectId,
+        $ProjectName,
 
         # Name of the forked repo
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
         [string]
+        [ValidateScript(
+            {
+                # Length
+                if (($_).Length -gt 63) {
+                    throw "The forked repo name cannot be longer than 63 characters"
+                }
+
+                # - Must not be a system reserved name.
+                $forbiddenNames = @(
+                    'COM1',
+                    'COM2',
+                    'COM3',
+                    'COM4',
+                    'COM5',
+                    'COM6',
+                    'COM7',
+                    'COM8',
+                    'COM9',
+                    'COM10',
+                    'CON',
+                    'DefaultCollection',
+                    'LPT1',
+                    'LPT2',
+                    'LPT3',
+                    'LPT4',
+                    'LPT5',
+                    'LPT6',
+                    'LPT7',
+                    'LPT8',
+                    'LPT9',
+                    'NUL',
+                    'PRN',
+                    'SERVER',
+                    'SignalR',
+                    'Web',
+                    'WEB')
+                if ($forbiddenNames -contains $_) {
+                    throw "The forked repo name cannot be one of the following values: $forbiddenNames"
+                }
+
+                # - Must not be one of the hidden segments used for IIS request filtering like App_Browsers, App_code, App_Data, App_GlobalResources, App_LocalResources, App_Themes, App_WebResources, bin, or web.config.
+                if ($_ -match "App_Browsers|App_code|App_Data|App_GlobalResources|App_LocalResources|App_Themes|App_WebResources|bin|web.config") {
+                    throw "The forked repo name cannot be one of the following values: App_Browsers, App_code, App_Data, App_GlobalResources, App_LocalResources, App_Themes, App_WebResources, bin, or web.config"
+                }
+
+                # - Must not contain any Unicode control characters or surrogate characters.
+                if ($_ -match "[\p{C}\p{Cs}]") {
+                    throw "The forked repo name cannot contain any Unicode control characters or surrogate characters"
+                }
+                # - Must not contain the following printable characters: \ / : * ? " < > | ; # $ * { } , + = [ ].
+                if ($_ -match "[\\/:*?`"<>|;#$*{},+=\[\]]") {
+                    throw "The forked repo name cannot contain the following printable characters: \ / : * ? `"< > | ; # $ * { } , + = [ ]"
+                }
+                # - Must not start with an underscore _.
+                if ($_ -match "^_") {
+                    throw "The forked repo name cannot start with an underscore _"
+                }
+                # - Must not start or end with a period ..
+                if ($_ -match "^\." -or $_ -match "\.$") {
+                    throw "The forked repo name cannot start or end with a period ."
+                }
+                $true
+            }
+        )]
         $ForkName,
 
-        # Azure DevOps Project-ID where the repo has to be forked from
+        # Azure DevOps Project-name where the fork has to be forked from
         [Parameter(Mandatory)]
         [string]
-        $SourceProjectId,
+        $SourceProjectName,
 
-        # Azure DevOps Repository-ID which should be forked
+        # Azure DevOps Repository-Name which should be forked
         [Parameter(Mandatory)]
         [string]
-        $SourceRepo,
+        $SourceRepoName,
 
         # If set to $true, a single branch specified in the ToBeForkedBranch-variable will be forked. If false will clone the repo with all branches
         [Parameter()]
@@ -68,31 +137,117 @@ function New-AzDoRepoFork {
         [string]
         $ToBeForkedBranch = 'main'
     )
-    if ($CopyBranch) {
-        $sourceRef = "sourceRef=refs/heads/$ToBeForkedBranch&"
-    }
-
-    $body = @{
-        name             = $ForkName
-        project          = @{
-            id = $ProjectId
+    begin {
+        if ($UsePAT) {
+            Write-Verbose 'The [UsePAT]-parameter was set to true, so the PAT will be used to authenticate with the organization.'
+            if ($PAT -eq $env:SYSTEM_ACCESSTOKEN) {
+                Write-Verbose -Message "Using the PAT from the environment variable 'SYSTEM_ACCESSTOKEN'."
+            } elseif (-not [string]::IsNullOrWhitespace($PAT) -and $PSBoundParameters.ContainsKey('PAT')) {
+                Write-Verbose -Message "Using a custom PAT supplied in the parameters."
+            } else {
+                try {
+                    throw "Requested to use a PAT, but no custom PAT was supplied in the parameters or the environment variable 'SYSTEM_ACCESSTOKEN' was not set."
+                } catch {
+                    $PSCmdlet.ThrowTerminatingError($_)
+                }
+            }
+        } else {
+            Write-Verbose 'The [UsePAT]-parameter was set to false, so an OAuth will be used to authenticate with the organization.'
+            $PAT = ($UsePAT ? $PAT : $null)
         }
-        parentRepository = @{
-            id      = $SourceRepo
-            project = @{
-                id = $SourceProjectId
+        try {
+            $Header = New-ADOAuthHeader -PAT $PAT -AccessToken:($UsePAT ? $false : $true) -ErrorAction Stop
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+
+        $getAzDoProjectSplat = @{
+            CollectionUri = $CollectionUri
+        }
+
+        if ($PAT) {
+            $getAzDoProjectSplat += @{
+                PAT    = $PAT
+                UsePAT = $true
             }
         }
+
+        $Projects = Get-AzDoProject @getAzDoProjectSplat
     }
 
-    $params = @{
-        uri         = "$CollectionUri/$OrganizationName/$ProjectId/_apis/git/repositories?${sourceRef}api-version=7.1-preview.1"
-        Method      = 'POST'
-        Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
-        body        = $Body | ConvertTo-Json -Depth 99
-        ContentType = 'application/json'
-    }
-    if ($PSCmdlet.ShouldProcess($CollectionUri)) {
-        Invoke-RestMethod @params
+    process {
+        $getAzDoRepoSplat = @{
+            CollectionUri = $CollectionUri
+            ProjectName   = $ProjectName
+        }
+
+        if ($PAT) {
+            $getAzDoRepoSplat += @{
+                PAT    = $PAT
+                UsePAT = $true
+            }
+        }
+
+        $repos = Get-AzDoRepo @getAzDoRepoSplat
+        $ProjectId = ($Projects | Where-Object ProjectName -EQ $ProjectName).Projectid
+        $SourceProjectId = ($Projects | Where-Object ProjectName -EQ $SourceProjectName).Projectid
+        $SourceRepoId = ($repos | Where-Object RepoName -EQ $SourceRepoName).RepoId
+
+        try {
+            if ($ForkName -in $repos.RepoName) {
+                Write-Error -Message "The repo name '$ForkName' already exists in the project '$ProjectName'."
+                throw
+            }
+        } catch {
+            continue
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+
+        if ($ForkName -like " *" -or $ForkName -like "* *" -or $ForkName -like "* ") {
+            Write-Warning -Message "The repo name '$name' contains spaces. It is strongly advised against to do so in Azure DevOps and git repositories. Please consider renaming the repo." -WarningAction Inquire
+        }
+
+
+        if ($CopyBranch) {
+            $sourceRef = "sourceRef=refs/heads/$ToBeForkedBranch&"
+        }
+
+        $body = @{
+            name             = $ForkName
+            project          = @{
+                id = $ProjectId
+            }
+            parentRepository = @{
+                id      = $SourceRepoId
+                project = @{
+                    id = $SourceProjectId
+                }
+            }
+        }
+
+        $params = @{
+            uri         = "$CollectionUri/$OrganizationName/$ProjectId/_apis/git/repositories?${sourceRef}api-version=7.1-preview.1"
+            Method      = 'POST'
+            Headers     = $Header
+            body        = $Body | ConvertTo-Json -Depth 99
+            ContentType = 'application/json'
+        }
+        if ($PSCmdlet.ShouldProcess($ProjectName, "Forking repo $($PSStyle.Bold)$SourceRepo$($PSStyle.Reset) named: $($PSStyle.Bold)$ForkName$($PSStyle.Reset) based on the branch $($PSStyle.Bold)$ToBeForkedBranch$($PSStyle.Reset)")) {
+            Invoke-RestMethod @params | ForEach-Object {
+                [PSCustomObject]@{
+                    RepoName      = $_.name
+                    RepoId        = $_.id
+                    RepoURL       = $_.url
+                    ProjectName   = $ProjectName
+                    WebUrl        = $_.webUrl
+                    HttpsUrl      = $_.remoteUrl
+                    SshUrl        = $_.sshUrl
+                    CollectionURI = $CollectionUri
+                }
+            }
+        } else {
+            $Body | Format-List
+            return
+        }
     }
 }
