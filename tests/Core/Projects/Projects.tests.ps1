937d602d5f5b818cc2e5d8bec471b2b8d1dea28f
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2023-12-05 09:09:15 +0100

    refactor: Projects.tests.ps1

--- tests/Core/Projects/Projects.tests.ps1
+++ tests/Core/Projects/Projects.tests.ps1
@@ -1,75 +1,77 @@
 BeforeDiscovery {
-  Get-Module "AzureDevOpsPowerShell" | Remove-Module -Force -ErrorAction Ignore
-  $path = Join-Path -Path $PSScriptRoot -ChildPath "..\..\..\AzureDevOpsPowerShell\AzureDevOpsPowerShell.psm1" | Resolve-Path
+  $ModuleName = 'AzureDevOpsPowerShell'
+  Get-Module $ModuleName | Remove-Module -Force -ErrorAction Ignore
+  $path = Join-Path -Path $PSScriptRoot -ChildPath "..\..\..\$ModuleName\$ModuleName.psm1" | Resolve-Path
   Import-Module -Name $path -Verbose:$false -ErrorAction Stop
-
 }
 
 BeforeAll {
   $collectionUri = "https://dev.azure.com/AzureDevOpsPowerShell"
 }
 
-Describe "Projects" {
-  Context "New-AzDoProject" {
-    BeforeAll {
-      $params = @{
-        CollectionUri = $collectionUri
-        ProjectName   = "ProjectTest"
+InModuleScope $ModuleName {
+  Describe "Projects" {
+    Context "New-AzDoProject" {
+      BeforeAll {
+        $params = @{
+          CollectionUri = $collectionUri
+          ProjectName   = "ProjectTest"
+        }
+  
+        $project = New-AzDoProject @params -Confirm:$false
       }
-
-      $project = New-AzDoProject @params -Confirm:$false
-    }
-
-    It "Creates a new project" {
-      $project.ProjectName | Should -Be "ProjectTest"
-    }
-
-    It "Creates a new project with the correct visibility" {
-      $project.ProjectVisibility | Should -Be "private"
-    }
-
-    It "Creates a new project with the correct state" {
-      $project.State | Should -Be "wellFormed"
-    }
-  }
-
-  Context "Get-AzDoProject" {
-    BeforeAll {
-      $params = @{
-        CollectionUri = $collectionUri
-        ProjectName   = "ProjectTest"
+  
+      It "Creates a new project" {
+        $project.ProjectName | Should -Be "ProjectTest"
       }
-
-      $project = Get-AzDoProject @params
-    }
-
-    It "Gets the correct project" {
-      $project.ProjectName | Should -Be "ProjectTest"
-    }
-
-    It "Gets the correct project with the correct visibility" {
-      $project.ProjectVisibility | Should -Be "private"
-    }
-
-    It "Gets the correct project with the correct state" {
-      $project.State | Should -Be "wellFormed"
-    }
-  }
-
-  Context "Remove-AzDoProject" {
-    BeforeAll {
-      $params = @{
-        CollectionUri = $collectionUri
-        ProjectName   = "ProjectTest"
+  
+      It "Creates a new project with the correct visibility" {
+        $project.ProjectVisibility | Should -Be "private"
       }
-
-      $project = Remove-AzDoProject @params -Confirm:$false
+  
+      It "Creates a new project with the correct state" {
+        $project.State | Should -Be "wellFormed"
+      }
     }
-
-    It "Removes the correct project" {
-      $project.id | Should -Not -Be $null
-      $project.status | Should -Not -Be $null
-      $project.url | Should -Not -Be $null
+  
+    Context "Get-AzDoProject" {
+      BeforeAll {
+        $params = @{
+          CollectionUri = $collectionUri
+          ProjectName   = "ProjectTest"
+        }
+  
+        $project = Get-AzDoProject @params
+      }
+  
+      It "Gets the correct project" {
+        $project.ProjectName | Should -Be "ProjectTest"
+      }
+  
+      It "Gets the correct project with the correct visibility" {
+        $project.ProjectVisibility | Should -Be "private"
+      }
+  
+      It "Gets the correct project with the correct state" {
+        $project.State | Should -Be "wellFormed"
+      }
+    }
+  
+    Context "Remove-AzDoProject" {
+      BeforeAll {
+        $params = @{
+          CollectionUri = $collectionUri
+          ProjectName   = "ProjectTest"
+        }
+  
+        $project = Remove-AzDoProject @params -Confirm:$false
+      }
+  
+      It "Removes the correct project" {
+        $project.id | Should -Not -Be $null
+        $project.status | Should -Not -Be $null
+        $project.url | Should -Not -Be $null
+      }
     }
   }
 }
