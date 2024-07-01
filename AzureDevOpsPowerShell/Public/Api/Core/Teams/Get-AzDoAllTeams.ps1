ae458d63a204e450b275fb3b28ed6a48f669bf08
Bram Poels <root@yuhnix.nl>
2024-07-01 10:22:08 +0200

    feat: added teams api

---
+++ AzureDevOpsPowerShell/Public/Api/Core/Teams/Get-AzDoAllTeams.ps1
@@ -0,0 +1,100 @@
+function Get-AzDoAllTeams {
+  <#
+  .SYNOPSIS
+      This script gets all teams within an organization.
+  .DESCRIPTION
+      This script retrieves all teams within an organization using the Azure DevOps REST API.
+  .EXAMPLE
+      $params = @{
+          CollectionUri = 'https://dev.azure.com/contoso/'
+          ExpandIdentity = $true
+          Mine = $true
+      }
+      Get-AzDoAllTeams @params
+
+      This example gets all teams within 'weareinspark' where the requesting user is a member.
+  .OUTPUTS
+      PSObject
+  .NOTES
+  #>
+  [CmdletBinding(SupportsShouldProcess)]
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateScript({ Validate-CollectionUri -CollectionUri $_ })]
+    [string]
+    $CollectionUri,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [bool]
+    $ExpandIdentity = $false,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [bool]
+    $Mine = $false,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [int]
+    $Skip = 0,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [int]
+    $Top = 0
+  )
+
+  begin {
+    $result = @()
+    Write-Verbose "Starting function: Get-AzDoAllTeams"
+
+    $queryParams = @()
+    if ($ExpandIdentity) {
+      $queryParams += "`$expandIdentity=$ExpandIdentity"
+    }
+    if ($Mine) {
+      $queryParams += "`$mine=$Mine"
+    }
+    if ($Skip -gt 0) {
+      $queryParams += "`$skip=$Skip"
+    }
+    if ($Top -gt 0) {
+      $queryParams += "`$top=$Top"
+    }
+    $queryParams = $queryParams -join "&"
+  }
+
+  process {
+    $uri = "$CollectionUri/_apis/teams"
+    $version = "7.1-preview.3"
+
+    $params = @{
+      Uri             = $uri
+      Version         = $version
+      QueryParameters = $queryParams
+      Method          = 'GET'
+    }
+
+    # Extract organization name from the last part of the CollectionUri
+    $Organization = $CollectionUri.Split('/')[-1]
+
+    if ($PSCmdlet.ShouldProcess($CollectionUri, "Get all teams in organization: $($PSStyle.Bold)$Organization$($PSStyle.Reset)")) {
+      $teams = (Invoke-AzDoRestMethod @params).value
+      $result += $teams
+    } else {
+      Write-Verbose "Calling Invoke-AzDoRestMethod with $($params | ConvertTo-Json -Depth 10)"
+    }
+  }
+
+  end {
+    if ($result) {
+      $result | ForEach-Object {
+        [PSCustomObject]@{
+          Organization = $Organization
+          TeamName     = $_.name
+          TeamId       = $_.id
+          Description  = $_.description
+          Url          = $_.url
+          IdentityUrl  = $_.identityUrl
+        }
+      }
+    }
+  }
+}
---
+++ AzureDevOpsPowerShell/Public/Api/Core/Teams/Get-AzDoProjectTeams.ps1
@@ -0,0 +1,84 @@
+function Get-AzDoProjectTeams {
+  <#
+      .SYNOPSIS
+          This script gets team details in a given project.
+      .DESCRIPTION
+          This script gets teams in a given project.
+          When used in a pipeline, you can use the pre-defined CollectionUri, ProjectName, and AccessToken (PAT) variables.
+      .EXAMPLE
+          $params = @{
+              CollectionUri = 'https://dev.azure.com/contoso/'
+              ProjectName = 'Project 1'
+              TeamName = 'testteam'
+          }
+          Get-AzDoProjectTeams @params
+
+          This example gets the team 'testteam' in 'Project 1'.
+      .EXAMPLE
+          $params = @{
+              CollectionUri = 'https://dev.azure.com/contoso/'
+              ProjectName = 'Project 1'
+          }
+          Get-AzDoProjectTeams @params
+
+          This example gets all teams in 'Project 1'.
+      .OUTPUTS
+          PSObject
+      .NOTES
+  #>
+  [CmdletBinding(SupportsShouldProcess)]
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateScript({ Validate-CollectionUri -CollectionUri $_ })]
+    [string]
+    $CollectionUri,
+
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [string]
+    $ProjectName,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $TeamName
+  )
+
+  begin {
+    $result = @()
+    Write-Verbose "Starting function: Get-AzDoProjectTeams"
+  }
+
+  process {
+    $params = @{
+      uri     = "$CollectionUri/_apis/projects/$ProjectName/teams"
+      version = "7.1-preview.3"
+      method  = 'GET'
+    }
+
+    if ($PSCmdlet.ShouldProcess($CollectionUri, "Get teams from: $($PSStyle.Bold)$ProjectName$($PSStyle.Reset)")) {
+      $teams = (Invoke-AzDoRestMethod @params).value
+      if ($TeamName) {
+        $result += $teams | Where-Object { $_.name -eq $TeamName }
+      } else {
+        $result += $teams
+      }
+    } else {
+      Write-Verbose "Calling Invoke-AzDoRestMethod with $($params | ConvertTo-Json -Depth 10)"
+    }
+  }
+
+  end {
+    if ($result) {
+      $result | ForEach-Object {
+        [PSCustomObject]@{
+          CollectionURI = $CollectionUri
+          ProjectName   = $ProjectName
+          TeamName      = $_.name
+          TeamId        = $_.id
+          Description   = $_.description
+          Url           = $_.url
+          IdentityUrl   = $_.identityUrl
+        }
+      }
+    }
+  }
+}
