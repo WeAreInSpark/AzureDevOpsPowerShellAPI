a3e5a72f7c0ad5bc05b583a845c8b03d73b814e6
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2024-12-06 17:04:27 +0100

    refactor: Improve New-PipelineRun function output structure

--- AzureDevOpsPowerShell/Public/Api/Pipelines/Runs/New-PipelineRun.ps1
+++ AzureDevOpsPowerShell/Public/Api/Pipelines/Runs/New-PipelineRun.ps1
@@ -7,15 +7,29 @@ function New-PipelineRun {
   It supports various parameters to customize the pipeline run, including the collection URI, project name, pipeline ID, branch, parameters, preview run flag, and stages to skip.
   This function leverages the Azure DevOps REST API to trigger the pipeline run.
 .EXAMPLE
-  New-PipelineRun -CollectionUri "https://dev.azure.com/organization" -ProjectName "SampleProject" -PipelineId 123 -StagesToSkip @("Stage1", "Stage2")
+  $newPipelineRunSplat = @{
+    CollectionUri = "https://dev.azure.com/organization"
+    ProjectName = "SampleProject"
+    PipelineId = 123
+    StagesToSkip = @("Stage1", "Stage2")
+  }
 
+  New-PipelineRun @newPipelineRunSplat
   This command initiates a new run of the pipeline with ID 123 in the "SampleProject" project.
 .EXAMPLE
-  New-PipelineRun -CollectionUri "https://dev.azure.com/organization" -ProjectName "SampleProject" -PipelineId 123 -Branch "dev" -Parameters @{param1 = "value1"; param2 = "value2"}
+  $newPipelineRunSplat = @{
+      CollectionUri = "https://dev.azure.com/organization"
+      ProjectName = "SampleProject"
+      PipelineId = 123
+      Branch = "dev"
+      Parameters = @{param1 = "value1"; param2 = "value2"}
+  }
 
+  New-PipelineRun @newPipelineRunSplat
   This command initiates a new run of the pipeline with ID 123 in the "SampleProject" project, targeting the "dev" branch.
 .OUTPUTS
-  System.Object
+  System.Management.Automation.PSCustomObject
+
   Returns the response from the Azure DevOps REST API, which includes details of the pipeline run.
 .NOTES
 #>
@@ -42,7 +56,7 @@ function New-PipelineRun {
 
     # If the run should be a preview run
     [Parameter(ValueFromPipelineByPropertyName)]
-    [bool]$PreviewRun,
+    [switch]$PreviewRun,
 
     # Stages to skip in the pipeline run
     [Parameter(ValueFromPipelineByPropertyName)]
@@ -52,14 +66,9 @@ function New-PipelineRun {
     [Parameter(ValueFromPipelineByPropertyName)]
     [string]$Branch
   )
-
-  begin {
-    Write-Verbose "Starting function: New-PipelineRun"
-
-    $body = @{}
-  }
-
   process {
+    $body = @{}
+
     if ($PreviewRun) {
       $body.Add("previewRun", $PreviewRun)
     }
@@ -93,13 +102,21 @@ function New-PipelineRun {
       Body    = $body
     }
 
-    if ($PSCmdlet.ShouldProcess("PiplineId: $PipelineId", "New run")) {
+    if ($PSCmdlet.ShouldProcess("PipelineId: $PipelineId", "Trigger a new run on pipeline")) {
       try {
-        $response = Invoke-AzDoRestMethod @params
-
-        $response
+        $output = Invoke-AzDoRestMethod @params
+        [PSCustomObject]@{
+          PipelineId    = $output.pipeline.id
+          RunId         = $output.id
+          RunName       = $output.name
+          CreatedAt     = $output.createdDate
+          ProjectName   = $ProjectName
+          CollectionUri = $CollectionUri
+          URL           = $output.url
+          Resources     = $output.resources
+        }
       } catch {
-        Write-AzDoError -message $_
+        Write-AzDoError -Message $_
       }
     } else {
       Write-Verbose "Calling Invoke-AzDoRestMethod with $($params| ConvertTo-Json -Depth 10)"
