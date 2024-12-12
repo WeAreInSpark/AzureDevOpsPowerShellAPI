f901895a9ae7cb27aea49f249441f1e7d0d120c9
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2024-12-12 17:06:51 +0100

    refactor: Simplify output handling and enhance parameter annotations in Get-AzDoProjectTeams

--- AzureDevOpsPowerShell/Public/Api/Core/Teams/Get-AzDoProjectTeams.ps1
+++ AzureDevOpsPowerShell/Public/Api/Core/Teams/Get-AzDoProjectTeams.ps1
@@ -1,40 +1,39 @@
 function Get-AzDoProjectTeams {
   <#
-      .SYNOPSIS
-          This script gets team details in a given project.
-      .DESCRIPTION
-          This script gets teams in a given project.
-          When used in a pipeline, you can use the pre-defined CollectionUri, ProjectName, and AccessToken (PAT) variables.
-      .EXAMPLE
-          $params = @{
-              CollectionUri = 'https://dev.azure.com/contoso/'
-              ProjectName = 'Project 1'
-              TeamName = 'testteam'
-          }
-          Get-AzDoProjectTeams @params
+  .SYNOPSIS
+      This script gets team details in a given project.
+  .DESCRIPTION
+      This script gets teams in a given project.
+      When used in a pipeline, you can use the pre-defined CollectionUri, ProjectName, and AccessToken (PAT) variables.
+  .EXAMPLE
+      $params = @{
+          CollectionUri = 'https://dev.azure.com/contoso/'
+          ProjectName = 'Project 1'
+          TeamName = 'testteam'
+      }
+      Get-AzDoProjectTeams @params
 
-          This example gets the team 'testteam' in 'Project 1'.
-      .EXAMPLE
-          $params = @{
-              CollectionUri = 'https://dev.azure.com/contoso/'
-              ProjectName = 'Project 1'
-          }
-          Get-AzDoProjectTeams @params
+      This example gets the team 'testteam' in 'Project 1'.
+  .EXAMPLE
+      $params = @{
+          CollectionUri = 'https://dev.azure.com/contoso/'
+          ProjectName = 'Project 1'
+      }
+      Get-AzDoProjectTeams @params
 
-          This example gets all teams in 'Project 1'.
-      .OUTPUTS
-          PSObject
-      .NOTES
+      This example gets all teams in 'Project 1'.
+  .OUTPUTS
+      PSObject
   #>
   [CmdletBinding(SupportsShouldProcess)]
   param (
-    # collection URI
+    # Collection URI
     [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
     [ValidateScript({ Validate-CollectionUri -CollectionUri $_ })]
     [string]
     $CollectionUri,
 
-    # project name of the project
+    # Project name of the project
     [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
     [string]
     $ProjectName,
@@ -44,13 +43,9 @@ function Get-AzDoProjectTeams {
     [string]
     $TeamName
   )
-
-  begin {
-    $result = @()
-    Write-Verbose "Starting function: Get-AzDoProjectTeams"
-  }
-
   process {
+    Write-Verbose "Starting function: Get-AzDoProjectTeams"
+
     $params = @{
       uri     = "$CollectionUri/_apis/projects/$ProjectName/teams"
       version = "7.1-preview.3"
@@ -60,28 +55,33 @@ function Get-AzDoProjectTeams {
     if ($PSCmdlet.ShouldProcess($CollectionUri, "Get teams from: $($PSStyle.Bold)$ProjectName$($PSStyle.Reset)")) {
       $teams = (Invoke-AzDoRestMethod @params).value
       if ($TeamName) {
-        $result += $teams | Where-Object { $_.name -eq $TeamName }
+        $teams | Where-Object { $_.name -eq $TeamName } | ForEach-Object {
+          [PSCustomObject]@{
+            CollectionURI = $CollectionUri
+            ProjectName   = $ProjectName
+            TeamName      = $_.name
+            TeamId        = $_.id
+            Description   = $_.description
+            Url           = $_.url
+            IdentityUrl   = $_.identityUrl
+          }
+        }
       } else {
-        $result += $teams
+        $teams | ForEach-Object {
+          [PSCustomObject]@{
+            CollectionURI = $CollectionUri
+            ProjectName   = $ProjectName
+            TeamName      = $_.name
+            TeamId        = $_.id
+            Description   = $_.description
+            Url           = $_.url
+            IdentityUrl   = $_.identityUrl
+          }
+        }
       }
     } else {
       Write-Verbose "Calling Invoke-AzDoRestMethod with $($params | ConvertTo-Json -Depth 10)"
     }
   }
-
-  end {
-    if ($result) {
-      $result | ForEach-Object {
-        [PSCustomObject]@{
-          CollectionURI = $CollectionUri
-          ProjectName   = $ProjectName
-          TeamName      = $_.name
-          TeamId        = $_.id
-          Description   = $_.description
-          Url           = $_.url
-          IdentityUrl   = $_.identityUrl
-        }
-      }
-    }
-  }
 }
+
