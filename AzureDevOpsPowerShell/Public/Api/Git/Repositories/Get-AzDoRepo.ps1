845548d83422e3b15fd6d8e16bc98dc647aa5536
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2024-12-16 11:51:54 +0100

    fix: Enhance Get-AzDoRepo function with improved verbosity and streamlined output structure

--- AzureDevOpsPowerShell/Public/Api/Git/Repositories/Get-AzDoRepo.ps1
+++ AzureDevOpsPowerShell/Public/Api/Git/Repositories/Get-AzDoRepo.ps1
@@ -53,11 +53,8 @@ function Get-AzDoRepo {
     $RepoName
   )
 
-  begin {
-    Write-Verbose "Starting function: Get-AzDoRepo"
-  }
-
   process {
+    Write-Verbose "Starting function: Get-AzDoRepo"
 
     $params = @{
       uri     = "$CollectionUri/$ProjectName/_apis/git/repositories"
@@ -75,34 +72,41 @@ function Get-AzDoRepo {
             Write-Error "Repo $name not found"
             continue
           } else {
-            $result += $repo
+            $repo | ForEach-Object {
+              [PSCustomObject]@{
+                CollectionURI = $CollectionUri
+                ProjectName   = $ProjectName
+                RepoName      = $_.name
+                RepoId        = $_.id
+                URL           = $_.url
+                DefaultBranch = $_.defaultBranch
+                WebUrl        = $_.webUrl
+                RemoteUrl     = $_.remoteUrl
+                SshUrl        = $_.sshUrl
+                IsDisabled    = $_.IsDisabled
+              }
+            }
           }
         }
       } else {
-        $result += $repos
+        $repos | ForEach-Object {
+          [PSCustomObject]@{
+            CollectionURI = $CollectionUri
+            ProjectName   = $ProjectName
+            RepoName      = $_.name
+            RepoId        = $_.id
+            URL           = $_.url
+            DefaultBranch = $_.defaultBranch
+            WebUrl        = $_.webUrl
+            RemoteUrl     = $_.remoteUrl
+            SshUrl        = $_.sshUrl
+            IsDisabled    = $_.IsDisabled
+          }
+        }
       }
 
     } else {
       Write-Verbose "Calling Invoke-AzDoRestMethod with $($params| ConvertTo-Json -Depth 10)"
-    }
-  }
-
-  end {
-    if ($result) {
-      $result | ForEach-Object {
-        [PSCustomObject]@{
-          CollectionURI = $CollectionUri
-          ProjectName   = $ProjectName
-          RepoName      = $_.name
-          RepoId        = $_.id
-          URL           = $_.url
-          DefaultBranch = $_.defaultBranch
-          WebUrl        = $_.webUrl
-          RemoteUrl     = $_.remoteUrl
-          SshUrl        = $_.sshUrl
-          IsDisabled    = $_.IsDisabled
-        }
-      }
     }
   }
 }
