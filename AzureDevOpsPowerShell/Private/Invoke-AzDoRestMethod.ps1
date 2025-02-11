d1e0a742f7afdec4bd9ad4641e17a133a4a44b55
Bas Wijdenes <baswijdenes@gmail.com>
2025-02-11 09:44:19 +0100

    Revert "updated Get-AzDoPull~  and Set-AzDoPull~"
    This reverts commit b7f5b02462fd82bb91912e21a635df991a33a117.

--- AzureDevOpsPowerShell/Private/Invoke-AzDoRestMethod.ps1
+++ AzureDevOpsPowerShell/Private/Invoke-AzDoRestMethod.ps1
@@ -82,9 +82,6 @@ function Invoke-AzDoRestMethod {
     }
 
     if ($QueryParameters) {
-      if ($QueryParameters -notlike "?*") {
-        $QueryParameters = "?$QueryParameters"
-      }
       $params.Uri = "$($Uri)?$($QueryParameters)&api-version=$($Version)"
     } else {
       $params.Uri = "$($Uri)?api-version=$($Version)"
