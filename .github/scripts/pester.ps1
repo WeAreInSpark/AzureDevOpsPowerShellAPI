a587db5af3d91f70101b7261fa1ad50535d3dde5
Bas Wijdenes <baswijdenes@gmail.com>
2025-02-28 10:45:12 +0100

    fix: Update Pester configuration to exclude specific test paths

--- .github/scripts/pester.ps1
+++ .github/scripts/pester.ps1
@@ -1,7 +1,9 @@
 $config = New-PesterConfiguration
 $config.Run.Path = @('./tests/')
-$config.Run.ExcludePath = @('./tests/pester.ps1', './tests/Indented.ScriptAnalyzerRules/')
+$config.Run.ExcludePath = @('./tests/Indented.ScriptAnalyzerRules/')
 $config.Run.Exit = $true
 $config.Output.CIFormat = 'GithubActions'
 $config.Output.Verbosity = 'Detailed'
 Invoke-Pester -Configuration $config
+
+# './tests/pester.ps1',
