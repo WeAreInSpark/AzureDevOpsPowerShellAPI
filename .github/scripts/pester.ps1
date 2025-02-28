206928a398d48ede100a9f97ccfd5f9de08969e4
Bas Wijdenes <baswijdenes@gmail.com>
2025-02-28 10:48:31 +0100

    fix: Update Pester configuration to exclude additional test paths

--- .github/scripts/pester.ps1
+++ .github/scripts/pester.ps1
@@ -1,9 +1,7 @@
 $config = New-PesterConfiguration
 $config.Run.Path = @('./tests/')
-$config.Run.ExcludePath = @('./tests/Indented.ScriptAnalyzerRules/')
+$config.Run.ExcludePath = @('./tests/pester.ps1', './tests/Indented.ScriptAnalyzerRules/')
 $config.Run.Exit = $true
 $config.Output.CIFormat = 'GithubActions'
 $config.Output.Verbosity = 'Detailed'
 Invoke-Pester -Configuration $config
-
-# './tests/pester.ps1',
--- build.ps1
+++ build.ps1
@@ -1,61 +1,72 @@
 [cmdletbinding(DefaultParameterSetName = 'Task')]
 param(
-    # Build task(s) to execute
-    [parameter(ParameterSetName = 'task', position = 0)]
-    [string[]]$Task = 'default',
+  # Build task(s) to execute
+  [parameter(ParameterSetName = 'task', position = 0)]
+  [string[]]$Task = 'default',
 
-    # Bootstrap dependencies
-    [switch]$Bootstrap,
+  # Bootstrap dependencies
+  [switch]$Bootstrap,
 
-    # Calculate version
-    [switch]$CalculateVersion,
+  # Calculate version
+  [switch]$CalculateVersion,
 
-    # List available build tasks
-    [parameter(ParameterSetName = 'Help')]
-    [switch]$Help,
+  # List available build tasks
+  [parameter(ParameterSetName = 'Help')]
+  [switch]$Help,
 
-    # Optional properties to pass to psake
-    [hashtable]$Properties,
+  # Optional properties to pass to psake
+  [hashtable]$Properties,
 
-    # Optional parameters to pass to psake
-    [hashtable]$Parameters = @{
-        "PSRepository"          = 'PSGallery'
-        "PSRepositoryApiKey"    = ""
-        "ScriptAnalysisEnabled" = $true
-        "TestEnabled"           = $true
-    }
+  # Optional parameters to pass to psake
+  [hashtable]$Parameters = @{
+    "PSRepository"          = 'PSGallery'
+    "PSRepositoryApiKey"    = ""
+    "ScriptAnalysisEnabled" = $true
+    "TestEnabled"           = $true
+  }
 )
 
 $ErrorActionPreference = 'Stop'
-
+'1'
 # Bootstrap dependencies
 if ($Bootstrap.IsPresent) {
-    Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    if ((Test-Path -Path ./requirements.psd1)) {
-        if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-            Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser -Force
-        }
-        Import-Module -Name PSDepend -Verbose:$false
-        Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
-    } else {
-        Write-Warning 'No [requirements.psd1] found. Skipping build dependency installation.'
+  '2'
+  Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+  '3'
+  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+  '4'
+  if ((Test-Path -Path ./requirements.psd1)) {
+    '5'
+    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+      '6'
+      Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser -Force
     }
+    '7'
+    Import-Module -Name PSDepend -Verbose:$false
+    '8'
+    Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
+  } else {
+    Write-Warning 'No [requirements.psd1] found. Skipping build dependency installation.'
+  }
 }
 
 # Execute psake task(s)
+'9'
 $psakeFile = './psakeFile.ps1'
 if ($PSCmdlet.ParameterSetName -eq 'Help') {
-    Get-PSakeScriptTasks -buildFile $psakeFile |
-    Format-Table -Property Name, Description, Alias, DependsOn
+  '10'
+  Get-PSakeScriptTasks -buildFile $psakeFile |
+  Format-Table -Property Name, Description, Alias, DependsOn
 } else {
-    Set-BuildEnvironment -Force
-    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -properties $Properties -parameters $Parameters
-    if ($psake.build_success) {
-        "Build complete"
-        exit 0
-    } else {
-        Write-Error "Build not complete"
-        exit 1
-    }
+  '11'
+  Set-BuildEnvironment -Force
+  '12'
+  Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -properties $Properties -parameters $Parameters
+  if ($psake.build_success) {
+    "Build complete"
+    exit 0
+  } else {
+    Write-Error "Build not complete"
+    exit 1
+  }
 }
